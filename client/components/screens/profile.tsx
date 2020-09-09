dd234a4e8c43b10d8c2046617c801f2ad3b60aed
Sebastian Hove <sebastianhove@gmail.com>
2020-09-09 16:41:20 -0500

    "Outline for user profile settings, rendering two components via state"

--- client/components/screens/profile.tsx
+++ client/components/screens/profile.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, Title } from 'react-native';
+import { normalize } from 'react-native-elements';
 
 export default function Profile({ navigation }) {
+  const [profile, setProfile] = useState(true);
+  const [getUser, setGetUser] = useState([]);
+  const onPress = () => {
+    setProfile(!profile);
+  };
+
   const pressHandler = () => {
     navigation.navigate('Profile');
   };
-  return (
-    <View>
-      <Text> Profile Screen </Text>
-      {/* <Button title='Go To Search Results' onPress={pressHandler}></Button> */}
-    </View>
-  );
+
+  const styles = StyleSheet.create({
+    title: {
+      fontSize: 32,
+      fontWeight: 'bold',
+    },
+  });
+
+  if (profile) {
+    return (
+      <View>
+        <Text style={styles.title}> Profile Page </Text>
+        <Text>Name: Your Name Here</Text>
+        <Text>Image: Image will go here</Text>
+        <Button title='Edit Profile' onPress={onPress} />
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Text style={styles.title}> Edit Your Profile </Text>
+
+        <Button title='Discard Changes' onPress={onPress} />
+      </View>
+    );
+  }
 }
